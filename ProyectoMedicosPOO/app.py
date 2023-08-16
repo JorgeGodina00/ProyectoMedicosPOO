49e8c11d60d46955bb2efc53554b32d0cba2b10b
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-08-15 18:51:26 -0600

    Se completan el login
    LOGIN terminado

--- ProyectoMedicosPOO/app.py
+++ ProyectoMedicosPOO/app.py
@@ -1,43 +1,66 @@
-from flask import Flask, render_template, request, redirect, url_for, flash #Importacion de librerias
-from flask_mysqldb import MySQL
+from flask import Flask
+from flask import render_template, request, session, redirect, Response,url_for, flash 
+from flask_mysqldb import MySQL, MySQLdb
 
 
-#iniciar servidor Flask
-#configuracion de BD
-app= Flask(__name__)
+app= Flask(__name__, template_folder='templates')
+
 app.config['MYSQL_HOST']="localhost"
 app.config['MYSQL_USER']="root"
 app.config['MYSQL_PASSWWORD']=""
 app.config['MYSQL_DB']="medicospoo"
-
-app.secret_key='mysecretkey'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
 
-#Declaracion de la ruta
-#Ruta index
-#La ruta se compone de la ruta y su funcion
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/guardar', methods=['POST'])
-def guardar():
-    if request.method == 'POST':
-        Vtitulo= request.form['txtTitulo']
-        Vartista= request.form['txtArtista']
-        Vanio= request.form['txtAnio']
-        #print()
-        CS = mysql.connection.cursor() #Variable de tipo cursor que contiene las herramientas paara realizar los querys
-        CS.execute("INSERT INTO albums (titulo, artista, anio) VALUES (%s, %s, %s)", (Vtitulo, Vartista, Vanio))
-        mysql.connection.commit()
-    flash('Album Agregado Correctamente')    
-    return redirect(url_for('index'))
+@app.route('/admin')
+def admin():
+     curSelect = mysql.connection.cursor()
+     curSelect.execute('SELECT * FROM medicos')
+     consulta = curSelect.fetchall()
+     return render_template('admindash.html', listMedi = consulta)
+ 
+@app.route("/agregarmedicos")
+def agregaadmin():
+     return render_template('guardarmedico.html')
 
-@app.route('/eliminar')
-def eliminar():
-    return "Se elimino"
+#LOGIN
+@app.route('/acceso-login', methods = ["GET", "POST"])
+def login():
+    
+    if request.method == 'POST' and 'txtRFC' in request.form and 'txtPass':
+        __rfc = request.form['txtRFC']
+        __pass = request.form['txtPass']
+        
+        cur=mysql.connection.cursor()
+        print("conexion a la BD exitosa")
+        cur.execute('SELECT * FROM medicos WHERE rfc = %s AND pass = %s', (__rfc,__pass))
+        account = cur.fetchone()
+        
+        if account:
+            session['logueado'] = True
+            session['rfc'] = account['rfc']
+            session['id_rol'] = account['id_rol']
+            
+            if session['id_rol'] == 1:
+                return render_template('admindash.html')
+            elif session['id_rol'] == 2:
+                return render_template('usuindex.html')
+        else:
+
+            return render_template('index.html', mensaje = "Usuario O Contraseña Incorrectos")
+
+@app.route('/salir')
+def salir():
+    return render_template('index.html')
+
+
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.secret_key='mysecretkey'
+    app.run(host = '0.0.0.0', port=5000, debug=True, threaded = True)
