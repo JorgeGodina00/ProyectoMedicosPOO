0605d3f096d61bce0ff2e90bff6227c557202f21
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-08-18 00:27:48 -0600

    cambios varios
    roman ya ponte a trabajar kbron

--- ProyectoMedicosPOO/app.py
+++ ProyectoMedicosPOO/app.py
@@ -22,11 +22,50 @@ def admin():
      curSelect = mysql.connection.cursor()
      curSelect.execute('SELECT * FROM medicos')
      consulta = curSelect.fetchall()
-     return render_template('admindash.html', listMedi = consulta)
- 
+     return render_template('admindash.html', listaMedicos = consulta)
+
+
 @app.route("/agregarmedicos")
 def agregaadmin():
      return render_template('guardarmedico.html')
+
+@app.route("/agregarpaciente")
+def agregarpaciente():
+    return render_template('agregarpacientes.html')
+
+
+@app.route("/agregarpacientes2", methods = ['POST'])
+def agregapacientes():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nacimiento = request.form['nacimiento']
+        enfermedades = request.form['enfermedades']
+        alergias = request.form['alergias']
+        antecendentes = request.form['antecendentes']
+        medico_rfc = request.form['medico_rfc']
+        
+        cs=mysql.connection.cursor()
+        cs.execute('INSERT INTO pacientes (nombre, nacimiento, enfermedades, alergias, antecedentes, medico_rfc) VALUES (%s, %s, %s, %s, %s, %s)', (nombre, nacimiento, enfermedades, alergias, antecendentes, medico_rfc))
+        mysql.connection.commit()
+        flash()
+    return render_template("agregarpacientes.html")
+ 
+@app.route('/registro_medico', methods=['POST'])
+def registro_medico():
+    if request.method == 'POST':
+        rfc = request.form['rfc']
+        cedula = request.form['cedula']
+        correo = request.form['correo']
+        id_rol = request.form['id_rol']
+        password = request.form['pass']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        
+        cs=mysql.connection.cursor()
+        cs.execute('INSERT INTO medicos (rfc, cedula, correo, id_rol, pass, nombre, apellido) VALUES (%s, %s, %s, %s, %s, %s, %s)', (rfc, cedula, correo, id_rol, password, nombre, apellido))
+        mysql.connection.commit()
+        flash('Album agregado correctamente :)')
+    return render_template('guardarmedico.html')
 
 #LOGIN
 @app.route('/acceso-login', methods = ["GET", "POST"])
